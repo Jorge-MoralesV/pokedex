314857024b06283ff526da8a32f0f888286239c4
Jorge-MoralesV <jorgemoralesv99@gmail.com>
2024-10-02 15:26:23 -0600

    Optimizacion de imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
 import { PokemonDetailsComponent } from './pages/pokemon-details/pokemon-details.component';
 import { RouterModule } from '@angular/router';
 
 
 import { FormsModule } from '@angular/forms';
-import { NgOptimizedImage } from '@angular/common';
 import { NavbarComponent } from './Components/Navbar/Navbar.component';
 import { LoadingComponent } from './Components/Loading/Loading.component';
 import { FooterComponent } from './Components/Footer/Footer.component';
@@ -30,7 +29,6 @@ import { FooterComponent } from './Components/Footer/Footer.component';
     HttpClientModule,
     RouterModule,
     FormsModule,
-    NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
